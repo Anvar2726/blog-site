a3b098cdcfe21221e30719d2fd709dc6d2626904
anvar2726 <xidiraliyevanvar@gmail.com>
2024-05-13 19:03:59 +0500

    ajskhdsf

--- src/schemas/postSchema.js
+++ src/schemas/postSchema.js
@@ -4,6 +4,7 @@ const postSchema = yup.object({
   title: yup.string().required(),
   description: yup.string().required(),
   category: yup.string().required(),
+  tags: yup.string().required()
 });
 
 export default postSchema;
