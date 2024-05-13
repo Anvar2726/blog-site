d36bced04de1db99b4f0898d6169901b33290ad7
anvar2726 <xidiraliyevanvar@gmail.com>
2024-05-13 18:09:17 +0500

    seclksjncks

---
+++ src/schemas/postSchema.js
@@ -0,0 +1,9 @@
+import * as yup from "yup";
+
+const postSchema = yup.object({
+  title: yup.string().required(),
+  description: yup.string().required(),
+  category: yup.string().required(),
+});
+
+export default postSchema;
--- src/server/request.js
+++ src/server/request.js
@@ -1,10 +1,16 @@
 import axios from "axios";
-import { ENDPOINT } from "../consts";
+import { ENDPOINT, TOKEN } from "../consts";
 import { toast } from "react-toastify";
+import Cookies from "js-cookie";
+
+const token = Cookies.get(TOKEN)
 
 const request = axios.create({
     baseURL: ENDPOINT,
     timeout: 10000,
+    headers: {
+        Authorization: `Bearer ${token}`
+    }
 })
 
 request.interceptors.response.use(response => response, (error) =>{
